43461935bdabe9e530b484bf01b1a217a46bb555
Nam-T <namthp99@gmail.com>
2020-11-18 00:58:48 +0700

    Sign up view

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "admin/homepage#index"
 
-  devise_for :accounts, skip: [:session, :password]
+  devise_for :accounts, skip: [:session, :password, :registrations]
   as :account do
-    get "login", to: "sessions#new", as: :new_account_session
-    post "login", to: "sessions#create", as: :account_session
+    get "sign_in", to: "sessions#new", as: :new_account_session
+    post "sign_in", to: "sessions#create", as: :account_session
     delete "logout", to: "sessions#destroy", as: :destroy_account_session
     get "forgot_password", to: "passwords#new", as: :new_account_password
     post "forgot_password", to: "passwords#create", as: :account_password
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
     put "forgot_password", to: "passwords#update"
     get "confirmation", to: "confirmations#show"
     put "confirmation", to: "confirmations#update"
+    get "sign_up", to: "registrations#new", as: :new_account_registration
+    post "sign_up", to: "registrations#create", as: :account_registration
   end
 end
