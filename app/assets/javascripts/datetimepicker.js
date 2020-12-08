e13f999fa39d04e09a7e03ce8675c827393a6462
Nam Vũ <namthp99@gmail.com>
2020-12-08 17:27:57 +0700

    Config datepicker

--- app/assets/javascripts/datetimepicker.js
+++ app/assets/javascripts/datetimepicker.js
@@ -1,4 +1,4 @@
-var array = ["2020-12-08","2020-12-07","2020-12-09"]
+var array = ["2020-12-08","2020-12-07","2020-12-09", "2020-12-10", "2020-12-06"]
 $(function() {
   $('.datepicker').datepicker({
     changeMonth: true,
@@ -11,6 +11,13 @@ $(function() {
       } else {
         return [false,"","unAvailable"];
       }
-    }
+    },
+    beforeShow: function (input, inst) {
+      setTimeout(function() {
+          inst.dpDiv.find('a.ui-state-highlight').removeClass('ui-state-highlight');
+      }, 100);
+    },
+    dayNamesMin: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
+    firstDay: 1
   });
 });
