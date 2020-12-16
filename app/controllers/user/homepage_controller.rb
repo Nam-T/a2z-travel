4342f4b63eda2822f4a79309e15791fc73943332
Nam-T <namthp99@gmail.com>
2020-12-16 17:55:29 +0700

    Fix paginate

--- app/controllers/user/homepage_controller.rb
+++ app/controllers/user/homepage_controller.rb
@@ -1,5 +1,5 @@
 class User::HomepageController < User::UserApplicationController
   def index
-    @journeys = Journey.all
+    @journeys = Journey.all.paginate page: params[:page], per_page: 12
   end
 end
